7f34bf51b98c01dbcdb5cd11780c0693ce13efee
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-09 10:31:22 +0530

    entity commit

---
+++ src/com/nick/springdemo/entity/Customer.java
@@ -0,0 +1,39 @@
+package com.nick.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@Data
+public class Customer {
+
+	public Customer(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+}
